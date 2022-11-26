b29f5d17b81a75023c0ef6404384f9ca0a0112ba
schmeb0704 <113418470+schmeb0704@users.noreply.github.com>
2022-11-26 03:00:42 +0000

    deleting unneeded files for this branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import Card from "./components/Card"
-import RoundedImgSrc from './components/RoundImgSrc'
 import ReacChildrenPrac from './components/ReactChildren'
 import "./index.css"
 
