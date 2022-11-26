98d5eca836e743a4018c4ba7e9182fb0b8c9624e
schmeb0704 <113418470+schmeb0704@users.noreply.github.com>
2022-11-26 02:53:33 +0000

    Practicing React Children pattern

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import Card from "./components/Card"
 import RoundedImgSrc from './components/RoundImgSrc'
+import ReacChildrenPrac from './components/ReactChildren'
+import "./index.css"
 
 function App() {
  return(
     <>
-      <RoundedImgSrc />
+      <ReacChildrenPrac />
     </>
  )
 }
